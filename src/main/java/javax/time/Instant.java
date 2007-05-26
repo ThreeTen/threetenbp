db13644e35a247c70462e5b2fcac7b5f3991bcdf
Stephen Colebourne <scolebourne@joda.org>
2007-05-26 21:22:29 +0000

    Initial checkin
    git-svn-id: https://threeten.svn.sourceforge.net/svnroot/threeten/branches/FLUENT_API/jsr-310-ri@30 291d795c-afe8-5c46-8ee5-bf9dd72e1864

---
+++ src/main/java/javax/time/Instant.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2007, Stephen Colebourne & Michael Nascimento Santos
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package javax.time;
+
+/**
+ * An instantaneous point on the time-line.
+ * 
+ * @author Stephen Colebourne
+ */
+public final class Instant {
+
+    /**
+     * The number of seconds from the epoch of 1970-01-01T00:00:00Z.
+     */
+    private final long seconds;
+    /**
+     * The number of nanoseconds, later along the time-line, from the seconds field.
+     * This is always positive, and never exceeds 999,999,999.
+     */
+    private final int nanos;
+
+    /**
+     * Factory method to create an instance of Instant using seconds from the
+     * epoch of 1970-01-01T00:00:00Z and nanosecond fraction of second.
+     * 
+     * @param seconds  the number of seconds from the epoch
+     * @param nanos  the nanoseconds within the second, must be positive
+     * @return the created Instant
+     */
+    public Instant instant(final long seconds, final int nanos) {
+        return new Instant(seconds, nanos);
+    }
+
+    /**
+     * Constructs an instance of Instant using seconds from the epoch of
+     * 1970-01-01T00:00:00Z and nanosecond fraction of second.
+     * 
+     * @param seconds  the number of seconds from the epoch
+     * @param nanos  the nanoseconds within the second, must be positive
+     */
+    private Instant(final long seconds, final int nanos) {
+        super();
+        this.seconds = seconds;
+        this.nanos = nanos;
+    }
+
+    /**
+     * Gets the number of seconds from the epoch.
+     * Points in time after the epoch are positive, earlier are negative.
+     * 
+     * @return the seconds from the epoch
+     */
+    public long getSeconds() {
+        return seconds;
+    }
+
+    /**
+     * Gets the number of nanoseconds, later along the time-line, from the start
+     * of the second returned by {@link #getSeconds()}.
+     * 
+     * @return the nanoseconds within the second, always positive, never exceeds 999,999,999
+     */
+    public int getNanos() {
+        return nanos;
+    }
+
+}
