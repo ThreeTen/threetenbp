12ead70628f70fd01b75f555f2f28390e036f97b
Michael Nascimento Santos <misterm@gmail.com>
2008-02-29 19:52:21 +0000

    TimeZone/ZonedDateTime support and javadoc fixes
    git-svn-id: https://threeten.svn.sourceforge.net/svnroot/threeten/trunk/jsr-310-ri@261 291d795c-afe8-5c46-8ee5-bf9dd72e1864

--- src/main/java/javax/time/Now.java
+++ src/main/java/javax/time/Now.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Stephen Colebourne & Michael Nascimento Santos
+ * Copyright (c) 2007, 2008, Stephen Colebourne & Michael Nascimento Santos
  *
  * All rights reserved.
  *
@@ -36,21 +36,27 @@ import java.io.Serializable;
 import javax.time.calendar.LocalDate;
 import javax.time.calendar.LocalDateTime;
 import javax.time.calendar.LocalTime;
+import javax.time.calendar.TimeZone;
 import javax.time.calendar.YearMonth;
+import javax.time.calendar.ZonedDateTime;
 import javax.time.calendar.field.Year;
 
 /**
  * A facade for accessing the current time in the Java Time Framework.
  *
+ * @author Michael Nascimento Santos
  * @author Stephen Colebourne
  */
 public abstract class Now {
-
     //-----------------------------------------------------------------------
     /**
      * Gets an instance of <code>Now</code> that obtains the current datetime
      * using the system millisecond clock - {@link System#currentTimeMillis()}
-     * and the default time zone - {@link java.util.TimeZone#getDefault()}.
+     * and the default time zone - {@link #timeZone()}.
+     * 
+     * All objects produced by this implementation have at best millisecond 
+     * precision, since this is the maximum precision supported by 
+     * <code>System</code>.
      *
      * @return an instance of now that uses the system clock in the default time zone
      */
@@ -62,26 +68,48 @@ public abstract class Now {
     /**
      * Gets an instance of <code>Instant</code> representing the current
      * instant on the time line.
-     *
-     * @return a Instant represnting the current instant, never null
+     * 
+     * The instant precision depends on the underlying implementation.
+     * 
+     * @return a <code>Instant</code> representing the current instant, never null
      */
     public abstract Instant instant();
 
     //-----------------------------------------------------------------------
     /**
+     * Gets the <code>TimeZone</code> instance used to produce <code>ZonedDateTime</code> instances.
+     * 
+     * @return the <code>TimeZone</code>
+     * @see #currentZonedDateTime()
+     * @see #setTimeZone(TimeZone)
+     */
+    public abstract TimeZone timeZone();
+
+    //-----------------------------------------------------------------------
+    /**
+     * Sets the <code>TimeZone</code> instance used to produce <code>ZonedDateTime</code> instances.
+     * 
+     * @param timeZone the new <code>TimeZone</code> instance, must not be null
+     * @throws IllegalArgumentException if <code>timeZone</code> is null
+     */
+    //TODO: Create a permission class and require it
+    public abstract void setTimeZone(TimeZone timeZone);
+
+    //-----------------------------------------------------------------------
+    /**
      * Gets an instance of <code>Year</code> representing the current year.
      *
-     * @return a year object represnting the current year, never null
+     * @return a year object representing the current year, never null
      */
     public Year currentYear() {
-        return null; //Year.currentYear();
+        return Year.isoYear(2007);
     }
 
     //-----------------------------------------------------------------------
     /**
      * Gets an instance of <code>YearMonth</code> representing the current month.
      *
-     * @return a month object represnting the current month, never null
+     * @return a month object representing the current month, never null
      */
     public YearMonth currentMonth() {
         return YearMonth.yearMonth(2007, 6);
@@ -89,27 +117,27 @@ public abstract class Now {
 
     //-----------------------------------------------------------------------
     /**
-     * Gets an instance of LocalDate representing today.
+     * Gets an instance of <code>LocalDate</code> representing today.
      *
-     * @return a day object represnting today, never null
+     * @return a day object representing today, never null
      */
     public LocalDate today() {
         return LocalDate.date(2007, 6, 1);
     }
 
     /**
-     * Gets an instance of LocalDate representing yesterday.
+     * Gets an instance of <code>LocalDate</code> representing yesterday.
      *
-     * @return a day object represnting yesterday, never null
+     * @return a day object representing yesterday, never null
      */
     public LocalDate yesterday() {
         return LocalDate.date(2007, 6, 1);
     }
 
     /**
-     * Gets an instance of LocalDate representing tomorrow.
+     * Gets an instance of <code>LocalDate</code> representing tomorrow.
      *
-     * @return a day object represnting tommorrow, never null
+     * @return a day object representing tommorrow, never null
      */
     public LocalDate tomorrow() {
         return LocalDate.date(2007, 6, 1);
@@ -117,9 +145,9 @@ public abstract class Now {
 
     //-----------------------------------------------------------------------
     /**
-     * Gets an instance of LocalTime representing the current time of day.
+     * Gets an instance of <code>LocalTime</code> representing the current time of day.
      *
-     * @return a time object represnting the current time of day, never null
+     * @return a time object representing the current time of day, never null
      */
     public LocalTime currentTime() {
         return LocalTime.time(12, 30);
@@ -127,12 +155,25 @@ public abstract class Now {
 
     //-----------------------------------------------------------------------
     /**
-     * Gets an instance of LocalDateTime representing the current date and time.
+     * Gets an instance of <code>LocalDateTime</code> representing the current date and time.
      *
-     * @return a date-time object represnting the current date and time, never null
+     * @return a date-time object representing the current date and time, never null
      */
     public LocalDateTime currentDateTime() {
         return LocalDateTime.dateTime(2007, 6, 1, 12, 30);
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Gets an instance of <code>ZonedDateTime</code> representing date and time in the default <code>TimeZone</code>.
+     * 
+     * It is a shortcut for <code>ZonedDateTime.dateTime(now.instant(), now.timeZone())</code> for this <code>Now</code> instance.
+     *
+     * @return a zoned date-time object representing date and time in the default <code>TimeZone</code>, never null
+     * @see #timeZone()
+     */
+    public ZonedDateTime currentZonedDateTime() {
+        return ZonedDateTime.dateTime(instant(), timeZone());
     }
 
     //-----------------------------------------------------------------------
@@ -149,6 +190,12 @@ public abstract class Now {
          * A serialization identifier for this class.
          */
         private static final long serialVersionUID = 1L;
+
+        /**
+         * The default <code>TimeZone</code>.
+         */
+        //TODO: Proper initialization
+        private transient TimeZone timeZone = TimeZone.UTC;
 
         /**
          * Restricted constructor.
@@ -173,6 +220,21 @@ public abstract class Now {
         public Instant instant() {
             return Instant.millisInstant(System.currentTimeMillis());
         }
-    }
 
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public TimeZone timeZone() {
+            return timeZone;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setTimeZone(TimeZone timeZone) {
+            this.timeZone = timeZone;
+        }
+    }
 }
