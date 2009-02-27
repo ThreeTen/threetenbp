d7547b0071422b51a85e9f163b7da8650541b8b4
Darryl West <dpw@raincitysoftware.com>
2009-02-27 03:16:39 +0000

    initial PeriodParser and tests
    git-svn-id: https://threeten.svn.sourceforge.net/svnroot/threeten/trunk/jsr-310-ri@710 291d795c-afe8-5c46-8ee5-bf9dd72e1864

---
+++ src/main/java/javax/time/period/PeriodParser.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright (c) 2008-2009, Stephen Colebourne & Michael Nascimento Santos
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package javax.time.period;
+
+// TODO : finish javadocs
+
+/**
+ * An immutable period parser that returns a Period parsed from a tokenized string.
+ *
+ * This is used to ...
+ *
+ * PeriodParser is immutable and thread-safe.
+ *
+ * @author Michael Nascimento Santos
+ * @author Stephen Colebourne
+ * @author Darryl West
+ */
+public class PeriodParser {
+	/**
+	 * used to validate the correct sequence of tokens
+	 */
+	private static final String TOKEN_SEQUENCE = "PYMDTHMS";
+    /**
+     * the standard string representing a zero period
+     */
+    public static final String ZERO = "PT0S";
+    
+    private static final PeriodParser instance = new PeriodParser();
+    
+    /**
+     * @param text string to be parsed
+     */
+    private PeriodParser() {
+    	
+    }
+    
+    public static PeriodParser getInstance() {
+    	return instance;
+    }
+    
+    /**
+     * Obtains an instance of <code>Period</code> from a string.
+     * <p>
+     * This will parse the string produced by <code>toString()</code> which is
+     * a subset of the ISO8601 period format <code>PnYnMnDTnHnMn.nS</code>.
+     * <p>
+     * The string consists of a series of numbers with a suffix identifying their meaning.
+     * The values, and suffixes, must be in the sequence year, month, day, hour, minute, second.
+     * Any of the number/suffix pairs may be omitted providing at least one is present.
+     * If the period is zero, the value is normally represented as <code>PT0S</code>.
+     * The numbers must consist of ASCII digits.
+     * Any of the numbers may be negative. Negative zero is not accepted.
+     * The number of nanoseconds is expressed as an optional fraction of the seconds.
+     * There must be at least one digit before any decimal point.
+     * There must be between 1 and 9 inclusive digits after any decimal point.
+     * The letters will all be accepted in upper or lower case.
+     * The decimal point may be either a dot or a comma.
+     *
+     * @return the created Period, never null
+     * @throws IllegalArgumentException if the text cannot be parsed to a Period
+     */
+    public Period parse(final String text) {
+    	ParseValues values = new ParseValues(text);
+        // force to upper case and coerce the comma to dot
+        String s = text.toUpperCase().replace(",", ".");
+        
+        // check for zero and skip parse
+        if (ZERO.equals(s)) {
+            return Period.ZERO;
+        }
+
+        try {
+            if (s.charAt(0) != 'P' || s.length() < 3 || s.contains("-0") || !validateOrdering(s)) {
+                throw new Exception("Invalid Period format");
+            }
+            
+            // strip off the leading P
+            String[] datetime = s.substring(1).split("T");
+            switch(datetime.length) {
+                case 2:
+                    parseDate(values, datetime[0]);
+                    parseTime(values, datetime[1]);
+                    break;
+                case 1:
+                    if (s.contains("T")) {
+                        parseTime(values, datetime[0]);
+                    } else {
+                        parseDate(values, datetime[0]);
+                    }
+            }
+            
+        } catch (Throwable t) {
+            throw new IllegalArgumentException("Text '" + text + "' cannot be parsed as a Period", t);
+        }
+        
+        return values.toPeriod();
+    }
+    
+    private void parseDate(ParseValues values, String s) throws Exception {
+    	values.index = 0;
+        while (values.index < s.length()) {
+            String value = parseNumber(values, s);
+            if (values.index < s.length()) {
+                char c = s.charAt(values.index);
+                switch(c) {
+                    case 'Y': values.years = parseInt(value) ; break;
+                    case 'M': values.months = parseInt(value) ; break;
+                    case 'D': values.days = parseInt(value) ; break;
+                    default:
+                    	throw new Exception("Period date parse format does not recognize: " + s.charAt(values.index));
+                }
+                
+                values.index++;
+            }
+        }
+    }
+    
+    private void parseTime(ParseValues values, String s) throws Exception {
+        values.index = 0;
+        s = prepareTime(s);
+        while (values.index < s.length()) {
+            String value = parseNumber(values, s);
+            if (values.index < s.length()) {
+                char c = s.charAt(values.index);
+                switch(c) {
+                    case 'H': values.hours = parseInt(value) ; break;
+                    case 'M': values.minutes = parseInt(value) ; break;
+                    case 'S': values.seconds = parseInt(value) ; break;
+                    case 'N': values.nanos = parseNanos(value); break;
+                    default:
+                    	throw new Exception("Period time parse format does not recognize: " + s.charAt(values.index));
+                }
+                
+                values.index++;
+            }
+        }
+    }
+    
+    private long parseNanos(String s) throws Exception {
+    	long n = 0L;
+
+    	if (s.length() > 9) {
+    		throw new Exception("Period parse nano second range exceeded for: " + s);
+    	}
+    	// pad to the right to create 10**9, then trim
+    	return Long.parseLong( (s + "000000000").substring(0, 9) );
+    }
+    
+    private String prepareTime(String s) throws Exception {
+        if (s.contains(".")) {
+        	int i = s.indexOf(".") + 1;
+        	// verify that a digit follows the decimal point
+        	if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
+        		throw new Exception("Decimal point must be followed by a digit");
+        	}
+            s = s.replace("S", "N").replace(".", "S");
+        }
+
+        return s;
+    }
+    
+    private int parseInt(String s) throws Exception {
+        return Integer.parseInt(s);
+    }
+    
+    private String parseNumber(ParseValues values, String s) {
+        
+        StringBuilder sb = new StringBuilder();
+        while (values.index < s.length()) {
+            char c = s.charAt(values.index);
+            if (Character.isDigit(c) || c == '-') {
+                sb.append(c);
+            } else {
+                break;
+            }
+            
+            values.index++;
+        }
+        
+        return sb.toString();
+    }
+    
+    private boolean validateOrdering(String s) {
+    	if (s == null) {
+    		return false;
+    	}
+    	
+    	int idx = 0;
+    	char[] chars = s.toCharArray();
+    	
+    	for (int i = 0; i < chars.length && idx < TOKEN_SEQUENCE.length(); i++) {
+    		char c = chars[i];
+    		if (Character.isLetter(c)) {
+    			int n = TOKEN_SEQUENCE.substring(idx).indexOf(c);
+    			if (n < 0) {
+    				return false;
+    			}
+    			
+    			idx += n;
+    		}
+    	}
+    	
+    	return true;
+    }
+    
+    /**
+     * Parse values container created for each parse
+     */
+    private class ParseValues {
+	    /**
+	     * The number of years.
+	     */
+	    int years;
+	    /**
+	     * The number of months.
+	     */
+	    int months;
+	    /**
+	     * The number of days.
+	     */
+	    int days;
+	    /**
+	     * The number of hours.
+	     */
+	    int hours;
+	    /**
+	     * The number of minutes.
+	     */
+	    int minutes;
+	    /**
+	     * The number of seconds.
+	     */
+	    int seconds;
+	    /**
+	     * The number of nanoseconds.
+	     */
+	    long nanos;
+	    
+	    /**
+	     * parser position index
+	     */
+	    int index;
+	    /**
+	     * the original text to be parsed
+	     */
+	    private final String text;
+	    
+	    ParseValues(final String text) {
+	    	if (text == null) {
+	    		throw new IllegalArgumentException("Parse text must not be null");
+	    	}
+	        this.text = text;
+	    }
+	    
+	    Period toPeriod() {
+	    	return Period.period(years, months, days, hours, minutes, seconds, nanos);
+	    }
+    }
+    
+}
---
+++ src/test/java/javax/time/period/TestPeriodParser.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright (c) 2008, Stephen Colebourne & Michael Nascimento Santos
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package javax.time.period;
+
+import static org.testng.Assert.assertEquals;
+
+import javax.time.Duration;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for PeriodParser.
+ *
+ * @author Darryl West
+ */
+@Test 
+public class TestPeriodParser {
+	
+	//-----------------------------------------------------------------------
+    // parse(String)
+    //-----------------------------------------------------------------------
+    @DataProvider(name="Parse")
+    Object[][] provider_factory_parse() {
+        return new Object[][] {
+            {"Pt0S", Period.ZERO},
+            {"pT0S", Period.ZERO},
+            {"PT0S", Period.ZERO},
+            {"Pt0s", Period.ZERO},
+            {"pt0s", Period.ZERO},
+            {"P0Y0M0DT0H0M0.0S", Period.ZERO},
+            
+            {"P1Y", Period.years(1)},
+            {"P100Y", Period.years(100)},
+            {"P-25Y", Period.years(-25)},
+            {"P" + Integer.MAX_VALUE + "Y", Period.years(Integer.MAX_VALUE)},
+            {"P" + Integer.MIN_VALUE + "Y", Period.years(Integer.MIN_VALUE)},
+            
+            {"P1M", Period.months(1)},
+            {"P0M", Period.months(0)},
+            {"P-1M", Period.months(-1)},
+            {"P" + Integer.MAX_VALUE + "M", Period.months(Integer.MAX_VALUE)},
+            {"P" + Integer.MIN_VALUE + "M", Period.months(Integer.MIN_VALUE)},
+            
+            {"P1D", Period.days(1)},
+            {"P0D", Period.days(0)},
+            {"P-1D", Period.days(-1)},
+            {"P" + Integer.MAX_VALUE + "D", Period.days(Integer.MAX_VALUE)},
+            {"P" + Integer.MIN_VALUE + "D", Period.days(Integer.MIN_VALUE)},
+            
+            {"P2Y3M25D", Period.yearsMonthsDays(2, 3, 25)},
+            
+            {"PT1H", Period.hours(1)},
+            {"PT-1H", Period.hours(-1)},
+            {"PT24H", Period.hours(24)},
+            {"PT-24H", Period.hours(-24)},
+            {"PT" + Integer.MAX_VALUE + "H", Period.hours(Integer.MAX_VALUE)},
+            {"PT" + Integer.MIN_VALUE + "H", Period.hours(Integer.MIN_VALUE)},
+            
+            {"PT1M", Period.minutes(1)},
+            {"PT-1M", Period.minutes(-1)},
+            {"PT60M", Period.minutes(60)},
+            {"PT-60M", Period.minutes(-60)},
+            {"PT" + Integer.MAX_VALUE + "M", Period.minutes(Integer.MAX_VALUE)},
+            {"PT" + Integer.MIN_VALUE + "M", Period.minutes(Integer.MIN_VALUE)},
+            
+            {"PT1S", Period.seconds(1)},
+            {"PT-1S", Period.seconds(-1)},
+            {"PT60S", Period.seconds(60)},
+            {"PT-60S", Period.seconds(-60)},
+            {"PT" + Integer.MAX_VALUE + "S", Period.seconds(Integer.MAX_VALUE)},
+            {"PT" + Integer.MIN_VALUE + "S", Period.seconds(Integer.MIN_VALUE)},
+            
+            {"PT1.1S", Period.period( 0, 0, 0, 0, 0, 1, 100000000 ) },
+            {"PT-1.1S", Period.period( 0, 0, 0, 0, 0, -1, 100000000 ) },
+            {"PT1.0001S", Period.seconds(1).plusNanos( 100000 ) },
+            {"PT1.0000001S", Period.seconds(1).plusNanos( 100 ) },
+            {"PT1.123456789S", Period.period( 0, 0, 0, 0, 0, 1, 123456789 ) },
+            {"PT1.999999999S", Period.period( 0, 0, 0, 0, 0, 1, 999999999 ) },
+
+        };
+    }
+    
+    @Test(dataProvider="Parse")
+    public void factory_parse(String text, Period expected) {
+    	Period p = PeriodParser.getInstance().parse(text);
+        assertEquals(p, expected);
+    }
+    
+    @Test(dataProvider="Parse")
+    public void factory_parse_comma(String text, Period expected) {
+    	if (text.contains(".")) {
+    		text = text.replace('.', ',');
+    		Period p = PeriodParser.getInstance().parse(text);
+        	assertEquals(p, expected);
+    	}
+    }
+    
+    @DataProvider(name="ParseFailures")
+    Object[][] provider_factory_parseFailures() {
+        return new Object[][] {
+            {""},
+            {"PTS"},
+            {"AT0S"},
+            {"PA0S"},
+            {"PT0A"},
+            
+            {"PT+S"},
+            {"PT-S"},
+            {"PT.S"},
+            {"PTAS"},
+            
+            {"PT+0S"},
+            {"PT-0S"},
+            {"PT+1S"},
+            {"PT-.S"},
+            
+            {"PT1ABC2S"},
+            {"PT1.1ABC2S"},
+            
+            {"PT123456789123456789123456789S"},
+            {"PT0.1234567891S"},
+            {"PT1.S"},
+            {"PT.1S"},
+            
+            {"PT2.-3"},
+            {"PT-2.-3"},
+        };
+    }
+
+    @Test(dataProvider="ParseFailures", expectedExceptions=IllegalArgumentException.class)
+    public void factory_parseFailures(String text) {
+    	PeriodParser.getInstance().parse(text);
+    }
+    
+
+    @Test(dataProvider="ParseFailures", expectedExceptions=IllegalArgumentException.class)
+    public void factory_parseFailures_comma(String text) {
+        text = text.replace('.', ',');
+        PeriodParser.getInstance().parse(text);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_tooBig() {
+    	String text = "PT" + Long.MAX_VALUE + "1S";
+    	PeriodParser.getInstance().parse(text);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_tooBig_decimal() {
+    	String text = "PT" + Long.MAX_VALUE + "1.1S";
+    	PeriodParser.getInstance().parse(text);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_tooSmall() {
+        String text = "PT" + Long.MIN_VALUE + "1S";
+        PeriodParser.getInstance().parse(text);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_tooSmall_decimal() {
+        String text = "PT" + Long.MIN_VALUE + ".1S";
+        PeriodParser.getInstance().parse(text);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_null() {
+    	PeriodParser.getInstance().parse(null);
+    }
+    
+    @DataProvider(name="ParseSequenceFailures")
+    Object[][] provider_factory_parseSequenceFailures() {
+        return new Object[][] {
+        	{"P0M0Y0DT0H0M0.0S"},
+        	{"P0M0D0YT0H0M0.0S"},
+        	{"P0S0D0YT0S0M0.0H"},
+        	{"PT0M0H0.0S"},
+        	{"PT0M0H"},
+        };
+    }
+    
+    @Test(dataProvider="ParseSequenceFailures", expectedExceptions=IllegalArgumentException.class)
+    public void factory_parse_badSequence(String text) {
+    	PeriodParser.getInstance().parse(text);
+    }
+}
