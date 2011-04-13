a1a0cee6cd07b2a059a4746d55615e100ab661f7
Stephen Colebourne <scolebourne@joda.org>
2011-04-13 14:39:45 +0000

    Add with methods to UTCInstant
    git-svn-id: https://threeten.svn.sourceforge.net/svnroot/threeten/trunk/threeten@1437 291d795c-afe8-5c46-8ee5-bf9dd72e1864

---
+++ src/test/java/javax/time/MockUTCRulesLeapOn1000.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2010, Stephen Colebourne & Michael Nascimento Santos
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of JSR-310 nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package javax.time;
+
+/**
+ * Mock rules that returns a leap second on day 1000.
+ *
+ * @author Stephen Colebourne
+ */
+public class MockUTCRulesLeapOn1000 extends UTCRules {
+
+    @Override
+    public String getName() {
+        return "Mock1000";
+    }
+
+    @Override
+    public int getLeapSecondAdjustment(long mjDay) {
+        return (mjDay == 1000 ? 1 : 0);
+    }
+
+    @Override
+    public int getTAIOffset(long mjDay) {
+        return (mjDay <= 1000 ? 10 : 11);
+    }
+
+    @Override
+    public long[] getLeapSecondDates() {
+        return new long[] {1000};
+    }
+
+    @Override
+    public TAIInstant convertToTAI(UTCInstant utcInstant) {
+        return null;
+    }
+
+    @Override
+    public UTCInstant convertToUTC(TAIInstant taiInstant) {
+        return null;
+    }
+
+}
