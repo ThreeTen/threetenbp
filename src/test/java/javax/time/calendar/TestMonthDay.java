b744a7a7f4c7a8c075e8e329e1434477e9754d28
Stephen Colebourne <scolebourne@joda.org>
2011-09-30 15:39:11 +0100

    Enhance test

--- src/test/java/javax/time/calendar/TestMonthDay.java
+++ src/test/java/javax/time/calendar/TestMonthDay.java
@@ -105,8 +105,10 @@ public class TestMonthDay {
         assertTrue(Modifier.isFinal(cls.getModifiers()));
         Field[] fields = cls.getDeclaredFields();
         for (Field field : fields) {
-            assertTrue(Modifier.isPrivate(field.getModifiers()));
-            assertTrue(Modifier.isFinal(field.getModifiers()));
+            if (Modifier.isStatic(field.getModifiers()) == false) {
+                assertTrue(Modifier.isPrivate(field.getModifiers()));
+                assertTrue(Modifier.isFinal(field.getModifiers()));
+            }
         }
     }
 
