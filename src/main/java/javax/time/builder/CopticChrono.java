84cf5ae93a351571af89ef18cf19ebbf6f15b945
Stephen Colebourne <scolebourne@joda.org>
2012-03-14 11:42:52 +0000

    Remove tabs

--- src/main/java/javax/time/builder/CopticChrono.java
+++ src/main/java/javax/time/builder/CopticChrono.java
@@ -28,8 +28,8 @@ import javax.time.chronology.StandardChronology;
  */
 public enum CopticChrono implements Chrono {
 
-	INSTANCE;
-	
+    INSTANCE;
+    
     /**
      * The minimum permitted year.
      */
@@ -56,17 +56,17 @@ public enum CopticChrono implements Chrono {
     private final long MAX_EPOCH_DAY = 0;
     
     private final int MONTHS_PER_YEAR = 13;
-	
-	@Override
-	public String getName() {
-		return "Coptic";
-	}
+    
+    @Override
+    public String getName() {
+        return "Coptic";
+    }
 
-	@Override
-	public DateTimeRuleRange getRange(DateTimeField field) {
-		if (field instanceof StandardDateTimeField) {
+    @Override
+    public DateTimeRuleRange getRange(DateTimeField field) {
+        if (field instanceof StandardDateTimeField) {
             switch ((StandardDateTimeField) field) {
-            	// FIXME: Should this represent Before/During Era of the Martyrs?
+                // FIXME: Should this represent Before/During Era of the Martyrs?
                 case ERA: return DateTimeRuleRange.of(MIN_YEAR, MAX_YEAR);   // TODO
                 case YEAR: return DateTimeRuleRange.of(MIN_YEAR, MAX_YEAR);
                 case YEAR_OF_ERA: return DateTimeRuleRange.of(1, MAX_YEAR);
@@ -85,31 +85,31 @@ public enum CopticChrono implements Chrono {
             }
         }
         return field.getImplementationRules(this).getRange(field);
-	}
+    }
 
-	@Override
-	public DateTimeRuleRange getRange(DateTimeField field, LocalDate date,
-			LocalTime time) {
-		if (field instanceof StandardDateTimeField) {
+    @Override
+    public DateTimeRuleRange getRange(DateTimeField field, LocalDate date,
+            LocalTime time) {
+        if (field instanceof StandardDateTimeField) {
             if (date != null) {
                 switch ((StandardDateTimeField) field) {
                     case DAY_OF_MONTH:
-                    	if(getMonthOfYear(date) == 13) {
-                    		return DateTimeRuleRange.of(1, date.isLeapYear() ? 6 : 5);
-                    	}
-                    	return DateTimeRuleRange.of(1, 30);
+                        if(getMonthOfYear(date) == 13) {
+                            return DateTimeRuleRange.of(1, date.isLeapYear() ? 6 : 5);
+                        }
+                        return DateTimeRuleRange.of(1, 30);
                     case DAY_OF_YEAR:
-                    	return date.isLeapYear() ? DateTimeRuleRange.of(1, 366) :  DateTimeRuleRange.of(1, 365);
+                        return date.isLeapYear() ? DateTimeRuleRange.of(1, 366) :  DateTimeRuleRange.of(1, 365);
                 }
             }
             return getRange(field);
         }
         return field.getImplementationRules(this).getRange(field, date, time);
-	}
+    }
 
-	@Override
-	public long getValue(DateTimeField field, LocalDate date, LocalTime time) {
-		if (field instanceof StandardDateTimeField) {
+    @Override
+    public long getValue(DateTimeField field, LocalDate date, LocalTime time) {
+        if (field instanceof StandardDateTimeField) {
             if (date != null) {
                 switch ((StandardDateTimeField) field) {
                     case ERA: return (date.getYear() > 0 ? 1 : 0);   // TODO
@@ -135,53 +135,53 @@ public enum CopticChrono implements Chrono {
             }
         }
         return Integer.MIN_VALUE;  // TODO: exception or quiet
-	}
-	
-	/**
-	 * Abstracted common logic, readability
-	 */
-	private long getMonthOfYear(LocalDate date) {
-		return 1 + (date.getDayOfYear() / 30);
-	}
-	
-	private long getDayOfMonth(LocalDate date) {
-		return date.getDayOfYear() - (getMonthOfYear(date) - 1) * 30;
-	}
+    }
+    
+    /**
+     * Abstracted common logic, readability
+     */
+    private long getMonthOfYear(LocalDate date) {
+        return 1 + (date.getDayOfYear() / 30);
+    }
+    
+    private long getDayOfMonth(LocalDate date) {
+        return date.getDayOfYear() - (getMonthOfYear(date) - 1) * 30;
+    }
 
-	@Override
-	public LocalDate setDate(DateTimeField field, LocalDate date, long newValue) {
-		if (field instanceof StandardDateTimeField) {
+    @Override
+    public LocalDate setDate(DateTimeField field, LocalDate date, long newValue) {
+        if (field instanceof StandardDateTimeField) {
             StandardDateTimeField std = (StandardDateTimeField) field;
             if (getRange(field, date, null).isValidValue(newValue) == false) {
                 throw new IllegalArgumentException();  // TODO
             }
             switch (std) {
-	        	case ERA: {   // TODO
-	                if ((date.getYear() > 0 && newValue == 0) && (date.getYear() <= 0 && newValue == 1)) {
-	                    return date.withYear(1 - date.getYear());
-	                }
-	                return date;
-	            }
-	        	case YEAR: return date.withYear(safeToInt(newValue));
-	            case YEAR_OF_ERA: new UnsupportedOperationException("Not implemented yet");
-	            case EPOCH_MONTH: return setMonthOfYear(date.withYear(safeToInt(newValue / MONTHS_PER_YEAR)),newValue % MONTHS_PER_YEAR);
-	            case MONTH_OF_YEAR: return setMonthOfYear(date,newValue);
-	            case EPOCH_DAY: return LocalDate.ofEpochDay(newValue);
-	            case DAY_OF_MONTH: return date.withDayOfYear(safeToInt((getMonthOfYear(date) - 1) * 30 + newValue));
-	            case DAY_OF_YEAR: return date.withDayOfYear(safeToInt(newValue));
-	            case DAY_OF_WEEK: return date.plusDays(newValue - date.getDayOfWeek().getValue());
-	        }
-		}
-		return field.getImplementationRules(this).setDate(field, date, newValue);
-	}
-	
-	/**
-	 * Abstracted common logic, readability
-	 */
-	private LocalDate setMonthOfYear(LocalDate date, long newValue) {
-		long dom = getDayOfMonth(date);
-		return date.withDayOfYear(safeToInt((newValue - 1) * 30 + dom));
-	}
+                case ERA: {   // TODO
+                    if ((date.getYear() > 0 && newValue == 0) && (date.getYear() <= 0 && newValue == 1)) {
+                        return date.withYear(1 - date.getYear());
+                    }
+                    return date;
+                }
+                case YEAR: return date.withYear(safeToInt(newValue));
+                case YEAR_OF_ERA: new UnsupportedOperationException("Not implemented yet");
+                case EPOCH_MONTH: return setMonthOfYear(date.withYear(safeToInt(newValue / MONTHS_PER_YEAR)),newValue % MONTHS_PER_YEAR);
+                case MONTH_OF_YEAR: return setMonthOfYear(date,newValue);
+                case EPOCH_DAY: return LocalDate.ofEpochDay(newValue);
+                case DAY_OF_MONTH: return date.withDayOfYear(safeToInt((getMonthOfYear(date) - 1) * 30 + newValue));
+                case DAY_OF_YEAR: return date.withDayOfYear(safeToInt(newValue));
+                case DAY_OF_WEEK: return date.plusDays(newValue - date.getDayOfWeek().getValue());
+            }
+        }
+        return field.getImplementationRules(this).setDate(field, date, newValue);
+    }
+    
+    /**
+     * Abstracted common logic, readability
+     */
+    private LocalDate setMonthOfYear(LocalDate date, long newValue) {
+        long dom = getDayOfMonth(date);
+        return date.withDayOfYear(safeToInt((newValue - 1) * 30 + dom));
+    }
 
     @Override
     public LocalTime setTime(DateTimeField field, LocalTime time, long newValue) {
@@ -202,10 +202,9 @@ public enum CopticChrono implements Chrono {
         return null;  // TODO
     }
 
-	@Override
-	public LocalDateTime setDateTime(DateTimeField field,
-			LocalDateTime dateTime, long newValue) {
-		if (field instanceof StandardDateTimeField) {
+    @Override
+    public LocalDateTime setDateTime(DateTimeField field, LocalDateTime dateTime, long newValue) {
+        if (field instanceof StandardDateTimeField) {
             StandardDateTimeField std = (StandardDateTimeField) field;
             if (std.isDateField()) {
                 return dateTime.with(setDate(field, dateTime.toLocalDate(), newValue));
@@ -214,85 +213,79 @@ public enum CopticChrono implements Chrono {
             }
         }
         return null;  // TODO
-	}
+    }
 
-	@Override
-	public LocalDate setDateLenient(DateTimeField field, LocalDate date,
-			long newValue) {
-		// TODO
-		return null;
-	}
+    @Override
+    public LocalDate setDateLenient(DateTimeField field, LocalDate date, long newValue) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	public LocalTime setTimeLenient(DateTimeField field, LocalTime time,
-			long newValue) {
-		// TODO
-		return null;
-	}
+    @Override
+    public LocalTime setTimeLenient(DateTimeField field, LocalTime time, long newValue) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	public LocalDateTime setDateTimeLenient(DateTimeField field,
-			LocalDateTime dateTime, long newValue) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public LocalDateTime setDateTimeLenient(DateTimeField field, LocalDateTime dateTime, long newValue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public LocalDate addToDate(DateTimeField field, LocalDate date, long amount) {
-		long oldValue = getValue(field, date, null);
-		return setDate(field, date, oldValue + amount);
-	}
+    @Override
+    public LocalDate addToDate(DateTimeField field, LocalDate date, long amount) {
+        long oldValue = getValue(field, date, null);
+        return setDate(field, date, oldValue + amount);
+    }
 
-	@Override
-	public LocalTime addToTime(DateTimeField field, LocalTime time, long amount) {
-		long oldValue = getValue(field, null, time);
-		return setTime(field, time, oldValue + amount);
-	}
+    @Override
+    public LocalTime addToTime(DateTimeField field, LocalTime time, long amount) {
+        long oldValue = getValue(field, null, time);
+        return setTime(field, time, oldValue + amount);
+    }
 
-	@Override
-	public LocalDateTime addToDateTime(DateTimeField field,
-			LocalDateTime dateTime, long amount) {
-		long oldValue = getValue(field, dateTime.toLocalDate(), dateTime.toLocalTime());
-		return setDateTime(field, dateTime, oldValue + amount);
-	}
+    @Override
+    public LocalDateTime addToDateTime(DateTimeField field, LocalDateTime dateTime, long amount) {
+        long oldValue = getValue(field, dateTime.toLocalDate(), dateTime.toLocalTime());
+        return setDateTime(field, dateTime, oldValue + amount);
+    }
 
-	@Override
-	public LocalDate rollDate(DateTimeField field, LocalDate date, long roll) {
-		// TODO
-		return null;
-	}
+    @Override
+    public LocalDate rollDate(DateTimeField field, LocalDate date, long roll) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	public LocalTime rollTime(DateTimeField field, LocalTime time, long roll) {
-		// TODO
-		return null;
-	}
+    @Override
+    public LocalTime rollTime(DateTimeField field, LocalTime time, long roll) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	public LocalDateTime rollDateTime(DateTimeField field,
-			LocalDateTime dateTime, long roll) {
-		// TODO
-		return null;
-	}
+    @Override
+    public LocalDateTime rollDateTime(DateTimeField field,
+            LocalDateTime dateTime, long roll) {
+        // TODO
+        return null;
+    }
 
-	@Override
-	public Duration getEstimatedDuration(PeriodUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Duration getEstimatedDuration(PeriodUnit unit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public Duration getDurationBetween(LocalDate date1, LocalTime time1,
-			LocalDate date2, LocalTime time2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Duration getDurationBetween(LocalDate date1, LocalTime time1, LocalDate date2, LocalTime time2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public long getPeriodBetween(PeriodUnit unit, LocalDate date1,
-			LocalTime time1, LocalDate date2, LocalTime time2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
+    @Override
+    public long getPeriodBetween(PeriodUnit unit, LocalDate date1, LocalTime time1, LocalDate date2, LocalTime time2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
 }
--- src/test/java/javax/time/builder/TestCopticChrono.java
+++ src/test/java/javax/time/builder/TestCopticChrono.java
@@ -16,62 +16,62 @@ import org.testng.annotations.Test;
 @Test
 public class TestCopticChrono {
 
-	CopticChrono chrono = CopticChrono.INSTANCE;
-	LocalDate march15 = LocalDate.of(2012, MARCH, 15);
-	LocalDate dec28 = LocalDate.of(2011, DECEMBER, 28);
-	LocalDate dec28leap = LocalDate.of(2012, DECEMBER, 28);
+    CopticChrono chrono = CopticChrono.INSTANCE;
+    LocalDate march15 = LocalDate.of(2012, MARCH, 15);
+    LocalDate dec28 = LocalDate.of(2011, DECEMBER, 28);
+    LocalDate dec28leap = LocalDate.of(2012, DECEMBER, 28);
 
-	@Test(groups = "tck")
-	public void monthRanges() {
-		DateTimeRuleRange monthRange = chrono.getRange(MONTH_OF_YEAR);
-		assertEquals(1, monthRange.getMinimum());
-		assertEquals(1, monthRange.getLargestMinimum());
-		assertEquals(13, monthRange.getMaximum());
-		assertEquals(13, monthRange.getSmallestMaximum());
-	}
+    @Test(groups = "tck")
+    public void monthRanges() {
+        DateTimeRuleRange monthRange = chrono.getRange(MONTH_OF_YEAR);
+        assertEquals(1, monthRange.getMinimum());
+        assertEquals(1, monthRange.getLargestMinimum());
+        assertEquals(13, monthRange.getMaximum());
+        assertEquals(13, monthRange.getSmallestMaximum());
+    }
 
-	@Test(groups = "tck")
-	public void dayRanges() {
-		DateTimeRuleRange monthRange = chrono.getRange(DAY_OF_MONTH);
-		assertEquals(1, monthRange.getMinimum());
-		assertEquals(1, monthRange.getLargestMinimum());
-		assertEquals(30, monthRange.getMaximum());
-		assertEquals(5, monthRange.getSmallestMaximum());
-	}
+    @Test(groups = "tck")
+    public void dayRanges() {
+        DateTimeRuleRange monthRange = chrono.getRange(DAY_OF_MONTH);
+        assertEquals(1, monthRange.getMinimum());
+        assertEquals(1, monthRange.getLargestMinimum());
+        assertEquals(30, monthRange.getMaximum());
+        assertEquals(5, monthRange.getSmallestMaximum());
+    }
 
-	@Test(groups = "tck")
-	public void specificRanges() {
-		assertEquals(chrono.getRange(DAY_OF_MONTH, march15, null).getMaximum(), 30);
-		assertEquals(chrono.getRange(DAY_OF_YEAR, march15, null).getMaximum(), 366);
+    @Test(groups = "tck")
+    public void specificRanges() {
+        assertEquals(chrono.getRange(DAY_OF_MONTH, march15, null).getMaximum(), 30);
+        assertEquals(chrono.getRange(DAY_OF_YEAR, march15, null).getMaximum(), 366);
 
-		assertEquals(chrono.getRange(DAY_OF_MONTH, dec28, null).getMaximum(), 5);
+        assertEquals(chrono.getRange(DAY_OF_MONTH, dec28, null).getMaximum(), 5);
 
-		assertEquals(chrono.getRange(DAY_OF_MONTH, dec28leap, null).getMaximum(), 6);
-	}
-	
-	@Test(groups = "tck")
-	public void getMonthsAndDays() {
-		assertEquals(chrono.getValue(MONTH_OF_YEAR, march15, null), 3);
-		assertEquals(chrono.getValue(MONTH_OF_YEAR, dec28, null), 13);
-		assertEquals(chrono.getValue(MONTH_OF_YEAR, dec28leap, null), 13);
-		
-		assertEquals(chrono.getValue(DAY_OF_MONTH, march15, null), 15);
-		assertEquals(chrono.getValue(DAY_OF_MONTH, dec28, null), 2);
-		assertEquals(chrono.getValue(DAY_OF_MONTH, dec28leap, null), 3);
-	}
-	
-	@Test(groups = "tck")
-	public void setMonthsAndDays() {
-		assertChronoEquals(2012, 3, 15, march15);
-		assertChronoEquals(2011, 13, 2, dec28);
-		assertChronoEquals(2012, 13, 3, dec28leap);
-	}
-	
-	void assertChronoEquals(int year, int month, int day, LocalDate isoDate) {
-		LocalDate value = chrono.setDate(YEAR, LocalDate.now(), year);
-		value = chrono.setDate(DAY_OF_MONTH, value, day);
-		value = chrono.setDate(MONTH_OF_YEAR, value, month);
-		assertEquals(isoDate, value);
-	}
+        assertEquals(chrono.getRange(DAY_OF_MONTH, dec28leap, null).getMaximum(), 6);
+    }
+    
+    @Test(groups = "tck")
+    public void getMonthsAndDays() {
+        assertEquals(chrono.getValue(MONTH_OF_YEAR, march15, null), 3);
+        assertEquals(chrono.getValue(MONTH_OF_YEAR, dec28, null), 13);
+        assertEquals(chrono.getValue(MONTH_OF_YEAR, dec28leap, null), 13);
+        
+        assertEquals(chrono.getValue(DAY_OF_MONTH, march15, null), 15);
+        assertEquals(chrono.getValue(DAY_OF_MONTH, dec28, null), 2);
+        assertEquals(chrono.getValue(DAY_OF_MONTH, dec28leap, null), 3);
+    }
+    
+    @Test(groups = "tck")
+    public void setMonthsAndDays() {
+        assertChronoEquals(2012, 3, 15, march15);
+        assertChronoEquals(2011, 13, 2, dec28);
+        assertChronoEquals(2012, 13, 3, dec28leap);
+    }
+    
+    void assertChronoEquals(int year, int month, int day, LocalDate isoDate) {
+        LocalDate value = chrono.setDate(YEAR, LocalDate.now(), year);
+        value = chrono.setDate(DAY_OF_MONTH, value, day);
+        value = chrono.setDate(MONTH_OF_YEAR, value, month);
+        assertEquals(isoDate, value);
+    }
 
 }
