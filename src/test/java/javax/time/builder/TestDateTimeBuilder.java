2c7ad27b54a6881c26ef33e6a7eacc931b2b3514
Richard Warburton <richard.warburton@gmail.com>
2012-03-24 12:06:22 +0000

    tidy imports

--- src/test/java/javax/time/builder/TestDateTimeBuilder.java
+++ src/test/java/javax/time/builder/TestDateTimeBuilder.java
@@ -12,13 +12,11 @@ import static javax.time.builder.StandardDateTimeField.MONTH_OF_YEAR;
 import static javax.time.builder.StandardDateTimeField.YEAR;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
 import javax.time.CalendricalException;
 import javax.time.LocalDate;
 
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
