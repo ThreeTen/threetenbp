c47e49447601f191e4a8b9df2441b0b5b29d47cd
Stephen Colebourne <scolebourne@joda.org>
2012-04-13 12:21:10 +0100

    Move unused resolution mode

--- src-extra/main/java/javax/time/extra/ResolutionMode.java
+++ src-extra/main/java/javax/time/extra/ResolutionMode.java
@@ -29,7 +29,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package javax.time.builder;
+package javax.time.extra;
 
 /**
  * How to resolve invalid combinations of date or time fields.
