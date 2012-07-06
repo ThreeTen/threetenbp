7a8be5c89e279bc400de232c2297eaa7dde05c4f
Stephen Colebourne <scolebourne@joda.org>
2012-07-06 14:01:24 +0100

    Add support for clock-hour
    Fixes #22

--- src/main/java/javax/time/LocalTime.java
+++ src/main/java/javax/time/LocalTime.java
@@ -393,19 +393,21 @@ public final class LocalTime
     public long get(DateTimeField field) {
         if (field instanceof LocalDateTimeField) {
             switch ((LocalDateTimeField) field) {
-                case NANO_OF_SECOND: return getNano();
+                case NANO_OF_SECOND: return nano;
                 case NANO_OF_DAY: return toNanoOfDay();
-                case MICRO_OF_SECOND: return getNano() / 1000;
+                case MICRO_OF_SECOND: return nano / 1000;
                 case MICRO_OF_DAY: return toNanoOfDay() / 1000;
-                case MILLI_OF_SECOND: return getNano() / 1000000;
+                case MILLI_OF_SECOND: return nano / 1000000;
                 case MILLI_OF_DAY: return toNanoOfDay() / 1000000;
-                case SECOND_OF_MINUTE: return getSecond();
+                case SECOND_OF_MINUTE: return second;
                 case SECOND_OF_DAY: return toSecondOfDay();
-                case MINUTE_OF_HOUR: return getMinute();
-                case MINUTE_OF_DAY: return getHour() * 60 + getMinute();
-                case HOUR_OF_AMPM: return getHour() % 12;
-                case HOUR_OF_DAY: return getHour();
-                case AMPM_OF_DAY: return getHour() / 12;
+                case MINUTE_OF_HOUR: return minute;
+                case MINUTE_OF_DAY: return hour * 60 + minute;
+                case HOUR_OF_AMPM: return hour % 12;
+                case CLOCK_HOUR_OF_AMPM: int ham = hour % 12; return (ham % 12 == 0 ? 12 : ham);
+                case HOUR_OF_DAY: return hour;
+                case CLOCK_HOUR_OF_DAY: return (hour == 0 ? 24 : hour);
+                case AMPM_OF_DAY: return hour / 12;
             }
             throw new CalendricalException(field.getName() + " not valid for LocalTime");
         }
@@ -477,10 +479,12 @@ public final class LocalTime
                 case SECOND_OF_MINUTE: return withSecond((int) newValue);
                 case SECOND_OF_DAY: return plusSeconds(newValue - toSecondOfDay());
                 case MINUTE_OF_HOUR: return withMinute((int) newValue);
-                case MINUTE_OF_DAY: return plusMinutes(newValue - (getHour() * 60 + getMinute()));
-                case HOUR_OF_AMPM: return plusHours(newValue - (getHour() % 12));
+                case MINUTE_OF_DAY: return plusMinutes(newValue - (hour * 60 + minute));
+                case HOUR_OF_AMPM: return plusHours(newValue - (hour % 12));
+                case CLOCK_HOUR_OF_AMPM: return plusHours((newValue == 12 ? 0 : newValue) - (hour % 12));
                 case HOUR_OF_DAY: return withHour((int) newValue);
-                case AMPM_OF_DAY: return plusHours((newValue - (getHour() / 12)) * 12);
+                case CLOCK_HOUR_OF_DAY: return withHour((int) (newValue == 24 ? 0 : newValue));
+                case AMPM_OF_DAY: return plusHours((newValue - (hour / 12)) * 12);
             }
             throw new CalendricalException(field.getName() + " not valid for LocalTime");
         }
--- src/test/java/javax/time/TestLocalTime.java
+++ src/test/java/javax/time/TestLocalTime.java
@@ -628,7 +628,12 @@ public class TestLocalTime {
         assertEquals(test.get(LocalDateTimeField.MINUTE_OF_HOUR), 30);
         assertEquals(test.get(LocalDateTimeField.SECOND_OF_MINUTE), 40);
         assertEquals(test.get(LocalDateTimeField.NANO_OF_SECOND), 987654321);
+        
+        assertEquals(test.get(LocalDateTimeField.SECOND_OF_DAY), 12 * 3600 + 30 * 60 + 40);
+        assertEquals(test.get(LocalDateTimeField.MINUTE_OF_DAY), 12 * 60 + 30);
         assertEquals(test.get(LocalDateTimeField.HOUR_OF_AMPM), 0);
+        assertEquals(test.get(LocalDateTimeField.CLOCK_HOUR_OF_AMPM), 12);
+        assertEquals(test.get(LocalDateTimeField.CLOCK_HOUR_OF_DAY), 12);
         assertEquals(test.get(LocalDateTimeField.AMPM_OF_DAY), AmPm.PM.getValue());
     }
 
