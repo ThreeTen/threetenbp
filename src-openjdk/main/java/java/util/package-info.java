a29f6d7477f6e5f95f2b2d5e66bcd66b4d83862b
Roger Riggs <roger.riggs@oracle.com>
2012-07-23 14:49:52 -0400

    Added description of changes to Date, Calendar, GregorianCalendar

--- src-openjdk/main/java/java/util/package-info.java
+++ src-openjdk/main/java/java/util/package-info.java
@@ -32,5 +32,24 @@
 
 /**
  * Amended OpenJDK classes that integrate JSR-310 with the existing date and time APIs.
+ * <p>
+ * {@link java.util.Date} is extended with conversions to and from {@link javax.time.Instant}
+ * with methods {@code java.util.Date.setInstant(Instant)},
+ * {@code java.util.Date(Instant)} to create a Date from an Instant, and
+ * {@code java.util.Date.toInstant()} to create an Instant from a Date.
+ * <p>
+ * {@link java.util.Calendar} is extended with conversions to and from {@link javax.time.Instant}
+ * with methods {@code java.util.Date.setInstant(Instant)} and
+ * {@code java.util.Calendar.toInstant()} to create an Instant from a Calendar instance.
+ * <p>
+ * {@link java.util.GregorianCalendar} is extended with conversions to and from the ISO Calendar
+ * API classes including {@link javax.time.LocalDate}, {@link javax.time.LocalTime},
+ * {@link javax.time.LocalDateTime}, {@link javax.time.OffsetDateTime},
+ * {@link javax.time.OffsetDate}, {@link javax.time.OffsetTime}, and
+ * {@link javax.time.ZonedDateTime}.
+ * GregorianCalendar implements the {@link javax.time.calendrical.DateTime} Interface,
+ * so its fields can be accessed with {@link javax.time.calendrical.DateTime#get},
+ * {@link javax.time.calendrical.DateTime#with}, {@link javax.time.calendrical.DateTime#extract}.
+ *
  */
 package java.util;
