8ca8199bf16555f8a14a433cfb03d7c82fba7091
Richard Warburton <richard.warburton@gmail.com>
2012-10-28 20:47:14 +0000

    split up TestISODate and move to TCK source folder

--- src-standard/test/java/javax/time/AllTest.java
+++ src-standard/test/java/javax/time/AllTest.java
@@ -45,7 +45,7 @@ import javax.time.calendrical.TestYearMonth;
 import javax.time.chrono.TestChronology;
 import javax.time.chrono.TestHijrahChronology;
 import javax.time.chrono.TestISOChronology;
-import javax.time.chrono.TestISODate;
+import javax.time.chrono.TestISODateImplementation;
 import javax.time.chrono.TestJapaneseChronology;
 import javax.time.chrono.TestMinguoChronology;
 import javax.time.chrono.TestThaiBuddhistChronology;
@@ -176,7 +176,7 @@ public class AllTest {
             TestChronology.class,
             TestHijrahChronology.class,
             TestISOChronology.class,
-            TestISODate.class,
+            TestISODateImplementation.class,
             TestJapaneseChronology.class,
             TestMinguoChronology.class,
             TestThaiBuddhistChronology.class,
