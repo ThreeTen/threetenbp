774b43686199c80ffe087dd6c176a09ad97d9255
Stephen Colebourne <scolebourne@joda.org>
2014-02-22 22:20:04 +0000

    Update TextStyle

--- src/main/java/org/threeten/bp/format/TextStyle.java
+++ src/main/java/org/threeten/bp/format/TextStyle.java
@@ -32,9 +32,21 @@
 package org.threeten.bp.format;
 
 /**
- * Enumeration of the style of text output to use.
+ * Enumeration of the style of text formatting and parsing.
  * <p>
- * This defines the "size" of the text to be output.
+ * Text styles define three sizes for the formatted text - 'full', 'short' and 'narrow'.
+ * Each of these three sizes is available in both 'standard' and 'stand-alone' variations.
+ * <p>
+ * The difference between the three sizes is obvious in most languages.
+ * For example, in English the 'full' month is 'January', the 'short' month is 'Jan'
+ * and the 'narrow' month is 'J'. Note that the narrow size is often not unique.
+ * For example, 'January', 'June' and 'July' all have the 'narrow' text 'J'.
+ * <p>
+ * The difference between the 'standard' and 'stand-alone' forms is trickier to describe
+ * as there is no difference in English. However, in other languages there is a difference
+ * in the word used when the text is used alone, as opposed to in a complete date.
+ * For example, the word used for a month when used alone in a date picker is different
+ * to the word used for month in association with a day and year in a date.
  *
  * <h3>Specification for implementors</h3>
  * This is immutable and thread-safe enum.
@@ -48,14 +60,56 @@ public enum TextStyle {
      */
     FULL,
     /**
+     * Full text for stand-alone use, typically the full description.
+     * For example, day-of-week Monday might output "Monday".
+     */
+    FULL_STANDALONE,
+    /**
      * Short text, typically an abbreviation.
      * For example, day-of-week Monday might output "Mon".
      */
     SHORT,
     /**
+     * Short text for stand-alone use, typically an abbreviation.
+     * For example, day-of-week Monday might output "Mon".
+     */
+    SHORT_STANDALONE,
+    /**
      * Narrow text, typically a single letter.
      * For example, day-of-week Monday might output "M".
      */
-    NARROW;
+    NARROW,
+    /**
+     * Narrow text for stand-alone use, typically a single letter.
+     * For example, day-of-week Monday might output "M".
+     */
+    NARROW_STANDALONE;
+
+    /**
+     * Checks if the style is stand-alone.
+     * 
+     * @return true if the style is stand-alone
+     */
+    public boolean isStandalone() {
+        return (ordinal() & 1) == 1;
+    }
+
+    /**
+     * Converts the style to the equivalent stand-alone style.
+     * 
+     * @return the matching stand-alone style
+     */
+    public TextStyle asStandalone() {
+        return TextStyle.values()[ordinal()  | 1];
+    }
+
+    /**
+     * Converts the style to the equivalent normal style.
+     *
+     * @return the matching normal style
+     */
+    public TextStyle asNormal() {
+        return TextStyle.values()[ordinal() & ~1];
+    }
 
 }
