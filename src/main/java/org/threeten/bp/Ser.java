3b4cc31cb1b215bf54f7a66eda3f48bf6db6b262
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-05-07 19:55:03 +1000

    import walkingkooka.j2cl.java.time.Externalizable
    - Replace java.io.Externalizable references because j2cl java.io.Externalizable is empty (missing readExternal)

--- src/main/java/org/threeten/bp/Ser.java
+++ src/main/java/org/threeten/bp/Ser.java
@@ -33,9 +33,10 @@ package org.threeten.bp;
 
 import javaemul.internal.annotations.GwtIncompatible;
 
+import walkingkooka.j2cl.java.time.Externalizable;
+
 import java.io.DataInput;
 import java.io.DataOutput;
-import java.io.Externalizable;
 import java.io.IOException;
 import java.io.InvalidClassException;
 import java.io.ObjectInput;
